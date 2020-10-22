8ab8fd7554e1f4d6440ce94cd980882b23f1cfd4
Ryan <mcculloughrt@gmail.com>
2020-10-22 14:54:40 -0700

    update dropper styling

--- src/App/Dropper/Dropper.tsx
+++ src/App/Dropper/Dropper.tsx
@@ -39,16 +39,23 @@ export default class Dropper extends React.Component<IDropperProps, IDropperStat
     }
 
     return (
-      <div className={ styles.dropzone } ref={this.dropRef}>
-        <div className={ styles.placeholder }>
-          <p>Drag glTF 2.0 file or folder here</p>
-        </div>
-        <div className={ styles.uploadBtn }>
-          <input ref={this.inputRef} type='file' name='file-input[]' id='file-input' multiple />
-          <label htmlFor='file-input'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17"><path d="M10 0L4.8 4.9h3.3V10h3.8V4.9h3.3L10 0zm9.3 11.5l-3.2-2.1h-2l3.4 2.6H14c-.1 0-.2.1-.2.1l-.8 2.3H7l-.8-2.2c-.1-.1-.1-.2-.2-.2H2.4l3.4-2.6h-2L.6 11.5c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9L20 13c.1-.5-.2-1.2-.7-1.5z"></path></svg>
-            <span>Upload</span>
-          </label>
+      <div style={{
+        display: 'flex',
+        flexGrow: 1,
+        height: '100%',
+        overflow: 'hidden'
+      }}>
+        <div className={ styles.dropzone } ref={this.dropRef}>
+          <div className={ styles.placeholder }>
+            <p>Drag glTF 2.0 file or folder here</p>
+          </div>
+          <div className={ styles.uploadBtn }>
+            <input ref={this.inputRef} type='file' name='file-input[]' id='file-input' multiple />
+            <label htmlFor='file-input'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17"><path d="M10 0L4.8 4.9h3.3V10h3.8V4.9h3.3L10 0zm9.3 11.5l-3.2-2.1h-2l3.4 2.6H14c-.1 0-.2.1-.2.1l-.8 2.3H7l-.8-2.2c-.1-.1-.1-.2-.2-.2H2.4l3.4-2.6h-2L.6 11.5c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9L20 13c.1-.5-.2-1.2-.7-1.5z"></path></svg>
+              <span>Upload</span>
+            </label>
+          </div>
         </div>
       </div>
     )
